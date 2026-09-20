976bf144cd33e170efb30e2043da0b4f9991282d
MokshithRao <111580947+MokshithRao@users.noreply.github.com>
2026-09-20 18:55:45 +0530

    Added update and delete endpoints.

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-api = FastAPI()
+api = FastAPI(title = "Task Manager API")
 
 class TaskRequest(BaseModel):
     id: int
     title: str
     status: bool
+
+
+class DeleteTask(BaseModel):
+    id: int
  
 
 @api.get("/")
@@ -36,12 +40,53 @@ def add_task(request:TaskRequest):
     id = request.id
     title = request.title
     status = request.status
+
     conn,penn = connect_mysql()
     penn.execute("insert into task(id, title, status) values (%s, %s, %s)", (id, title, status))
+
     conn.commit()
     conn.close()
+
     print(request)
+
     return "Task added successfully"
 
 
+
+@api.put("/update")
+def update_task(request: TaskRequest):
+    id = request.id
+    title = request.title
+    status = request.status
+
+    conn, penn = connect_mysql()
+
+    penn.execute(
+        "update task set title = %s, status = %s where id = %s", 
+        (title, status, id)
+    )
+
+    conn.commit()
+    conn.close()
+
+    return "Task updated successfully"
+
+
+
+
+@api.delete("/delete")
+def delete_task(request: DeleteTask):
+    id = request.id
+
+    conn, penn = connect_mysql()
+
+    penn.execute(
+        "delete from task where id = %s",
+        (id,)
+    )
+
+    conn.commit()
+    conn.close()
+
+    return "Task deleted successfully"
 
